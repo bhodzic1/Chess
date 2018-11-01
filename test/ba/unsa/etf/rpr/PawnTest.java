0bc6e9c49e9a0e637172276ee32afcaec6f42a0e
Bernes Hodzic <bhodzic1@etf.unsa.ba>
2018-11-02 00:09:02 +0100

    Testovi za pijuna

--- test/ba/unsa/etf/rpr/PawnTest.java
+++ test/ba/unsa/etf/rpr/PawnTest.java
@@ -11,5 +11,25 @@ class PawnTest {
                 () -> p.move("E4")
         );
     }
-
+    @org.junit.jupiter.api.Test
+    void move2() {
+        Pawn p = new Pawn("A2", ChessPiece.Color.WHITE);
+        assertDoesNotThrow(
+                () -> p.move("A3")
+        );
+    }
+    @org.junit.jupiter.api.Test
+    void moveDiagonal() {
+        Pawn p = new Pawn("E2", ChessPiece.Color.WHITE);
+        assertThrows( IllegalChessMoveException.class,
+                () -> p.move("F4")
+        );
+    }
+    @org.junit.jupiter.api.Test
+    void moveBack() {
+        Pawn p = new Pawn("A2", ChessPiece.Color.WHITE);
+        assertThrows(IllegalChessMoveException.class,
+                () -> p.move("A1")
+        );
+    }
 }
